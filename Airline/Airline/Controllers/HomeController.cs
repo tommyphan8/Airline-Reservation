54b0a976db44c8a25898f5bc4f66cebba4d6814c
Tommy Phan <tommyphan8@csu.fullerton.edu>
2015-04-13 23:29:55 -0700

    first commit

---
+++ Airline/Airline/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Airline.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Welcome, to your new MVC application!";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
