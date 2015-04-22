90f3f036addc6bd2a0a346d1856d66dd494506dc
linhcao1611 <linhcao@csu.fullerton.edu>
2015-04-21 22:43:33 -0700

    Update view + controller for booking ticket
    still working on pass fid and pid around

--- FlightReservation/FlightReservation/Controllers/PassengerController.cs
+++ FlightReservation/FlightReservation/Controllers/PassengerController.cs
@@ -10,137 +10,107 @@ using FlightReservation.Models;
 
 namespace FlightReservation.Controllers
 {
-    public class TicketController : Controller
+    public class PassengerController : Controller
     {
         private db_9c079b_airlineEntities db = new db_9c079b_airlineEntities();
 
-        // GET: /Ticket/
+        // GET: /Passenger/
         public ActionResult Index()
         {
-            return View(db.tickets.ToList());
+            return View(db.passengers.ToList());
         }
 
-        public ActionResult SearchInput()
-        {
-            return View();
-        }
-
-        public ActionResult SearchResult(FormCollection collection)
-        {
-            //string date = collection.Get("dDate");
-            DateTime dDate = Convert.ToDateTime(collection.Get("dDate"));
-            DateTime aDate = Convert.ToDateTime(collection.Get("aDate"));
-            string dAirport = collection.Get("dAirport");
-            string aAirport = collection.Get("aAirport");
-
-            //DateTime dDate = Convert.ToDateTime(collection.Get("dDate"));
-            //string dAirport = collection.Get("dAirport");
-            var flight = from f in db.flights select f;
-
-            //flight = flight.Where(s => s.Departs.Contains(dAirport));
-            //flight = flight.Where(s => s.Dtime => collection.Get("dDate"));
-            flight = flight.Where(s => s.Dtime >= dDate)
-            .Where(s => s.Departs.Contains(dAirport))
-            .Where(s => s.Departs.Contains(dAirport));
-            //flight = flight.Where(s => s.Atime >= aDate);
-
-
-
-            return View(flight);
-        }
-
-
-        // GET: /Ticket/Details/5
+        // GET: /Passenger/Details/5
         public ActionResult Details(long? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            ticket ticket = db.tickets.Find(id);
-            if (ticket == null)
+            passenger passenger = db.passengers.Find(id);
+            if (passenger == null)
             {
                 return HttpNotFound();
             }
-            return View(ticket);
+            return View(passenger);
         }
 
-        // GET: /Ticket/Create
-        public ActionResult Create(string Fid)
+        // GET: /Passenger/Create
+        public ActionResult Create()
         {
             return View();
         }
 
-        // POST: /Ticket/Create
+        // POST: /Passenger/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include="Tid,FinalPrice,Status,SeatNum,SeatClass,Fid,Pid,Num_of_bags")] ticket ticket)
+        public ActionResult Create([Bind(Include="Pid,FName,Mname,LName,DOB,Gender,Phone")] passenger passenger)
         {
             if (ModelState.IsValid)
             {
-                db.tickets.Add(ticket);
+                db.passengers.Add(passenger);
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
 
-            return View(ticket);
+            return View(passenger);
         }
 
-        // GET: /Ticket/Edit/5
+        // GET: /Passenger/Edit/5
         public ActionResult Edit(long? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            ticket ticket = db.tickets.Find(id);
-            if (ticket == null)
+            passenger passenger = db.passengers.Find(id);
+            if (passenger == null)
             {
                 return HttpNotFound();
             }
-            return View(ticket);
+            return View(passenger);
         }
 
-        // POST: /Ticket/Edit/5
+        // POST: /Passenger/Edit/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include="Tid,FinalPrice,Status,SeatNum,SeatClass,Fid,Pid,Num_of_bags")] ticket ticket)
+        public ActionResult Edit([Bind(Include="Pid,FName,Mname,LName,DOB,Gender,Phone")] passenger passenger)
         {
             if (ModelState.IsValid)
             {
-                db.Entry(ticket).State = EntityState.Modified;
+                db.Entry(passenger).State = EntityState.Modified;
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
-            return View(ticket);
+            return View(passenger);
         }
 
-        // GET: /Ticket/Delete/5
+        // GET: /Passenger/Delete/5
         public ActionResult Delete(long? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            ticket ticket = db.tickets.Find(id);
-            if (ticket == null)
+            passenger passenger = db.passengers.Find(id);
+            if (passenger == null)
             {
                 return HttpNotFound();
             }
-            return View(ticket);
+            return View(passenger);
         }
 
-        // POST: /Ticket/Delete/5
+        // POST: /Passenger/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(long id)
         {
-            ticket ticket = db.tickets.Find(id);
-            db.tickets.Remove(ticket);
+            passenger passenger = db.passengers.Find(id);
+            db.passengers.Remove(passenger);
             db.SaveChanges();
             return RedirectToAction("Index");
         }
