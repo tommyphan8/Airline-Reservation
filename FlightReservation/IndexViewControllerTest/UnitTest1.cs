2fe7c643576a5de695818a9d160f46ef68458274
kourun <kourun@csu.fullerton.edu>
2015-04-28 12:28:25 -0700

    Unittesting for home vieew
    add a unit testing project to test the home view if it route correctly.
    the test for flight view is in complete since there are no connection
    string to the db , so it always return a fail as expected. to unit test
    a database need to add a database unit testing , not required.

---
+++ FlightReservation/IndexViewControllerTest/UnitTest1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using FlightReservation;
+using FlightReservation.Controllers;
+
+
+namespace IndexViewControllerTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void IndexTestUnit()
+        {
+
+            //Arrange
+            HomeController Control1 = new HomeController();
+            //Act
+            ViewResult result = Control1.Index() as ViewResult;
+            //Assert
+            Assert.IsNotNull(result);
+
+        }
+        //Passed 
+        [TestMethod]
+        public void AboutTestUnit()
+        {
+            //Arrange
+            HomeController Control1 = new HomeController();
+            //Act
+            ViewResult result = Control1.About() as ViewResult;
+            //Assert
+            Assert.IsNotNull(result);
+
+
+        }
+        //Passed 
+        [TestMethod]
+        public void ContactTestUnit()
+        {
+            //Arrange
+            HomeController Control1 = new HomeController();
+            //Act
+            ViewResult result = Control1.Contact() as ViewResult;
+            //Assert
+            Assert.IsNotNull(result);
+        }
+        /*
+         * //Test Failed As expected since there are no connection string Provided
+         * //And the The mock up database set up would be too hard to merge in with 
+         * the original code for the time frame
+        [TestMethod]
+        public void FlightViewTest()
+        {
+            flights1Controller Control = new flights1Controller();
+            ViewResult result = Control.Getflight(0) as ViewResult;
+            Assert.IsNotNull(result);
+
+        }*/
+        
+    }
+}
